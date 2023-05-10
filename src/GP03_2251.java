5d91191c5d036e7e9b4ed992aac305673bf414b9
willsk-kim <willsk.kim@gmail.com>
2023-05-10 18:09:02 +0900

    add water moving

---
+++ src/GP03_2251.java
@@ -0,0 +1,84 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/*
+물통
+
+ */
+
+class State {
+    int[] X;
+    State(int[] _X) {
+        X = new int[3];
+        for(int i = 0; i < 3; i++) X[i] = _X[i];
+    }
+    State move(int from, int to, int[] Limit){
+        // from -> to 물통으로 물을 옮긴다.
+        int[] nX = new int[]{X[0], X[1], X[2]}; // 새로운 상태 설정
+        if(X[from] + X[to] <= Limit[to]){ //
+            nX[to] = nX[from] + nX[to];
+            nX[from] = 0;
+        } else {
+            nX[from] -= Limit[to] -nX[to];
+            nX[to] = Limit[to];
+        }
+
+        return new State(nX);
+
+    }
+}
+public class GP03_2251 {
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] Limit; // 물통 크기
+    static boolean[] possible; // 정답 체크
+    static boolean[][][] visit; // 방문 여부 체크
+
+    static void input() {
+        Limit = new int[3];
+        for(int i = 0; i < 3; i++) Limit[i] = sc.nextInt();
+        visit = new boolean[205][205][205];
+        possible = new boolean[205];
+    }
+
+    static void bfs(int x1, int x2, int x3){
+        Queue<State> Q = new LinkedList<>();
+        visit[x1][x2][x3] = true;
+        Q.add(new State(new int[]{x1, x2, x3}));
+
+        // start bfs
+        while(!Q.isEmpty()){
+            State st = Q.poll();
+            if(st.X[0] == 0) possible[st.X[2]] = true; // 첫번째 물토이 비어 있는 케이스 정답 체크
+            for(int from = 0; from < 3; from++){
+                for(int to = 0; to < 3; to++){
+                    if(from == to) continue;
+                    // 물통 들어서 물 옮긴다.
+                    State nst = st.move(from, to, Limit);
+
+                    if(!visit[nst.X[0]][nst.X[1]][nst.X[2]]){
+                        visit[nst.X[0]][nst.X[1]][nst.X[2]] = true;
+                        Q.add(nst);
+                    }
+                }
+            }
+        }
+
+    }
+
+    static void proCalc(){
+        bfs(0, 0, Limit[2]);
+        for(int i = 0 ; i <= 200; i++){
+            if(possible[i]) sb.append(i).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args){
+        input();
+        proCalc();
+    }
+
+}

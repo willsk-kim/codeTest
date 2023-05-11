34215eb099ca8604911d6755f4eace47db888de0
willsk-kim <willsk.kim@gmail.com>
2023-05-11 10:03:56 +0900

    escape miro

--- src/GP04_14502.java
+++ src/GP04_14502.java
@@ -3,27 +3,27 @@ import java.util.Queue;
 import java.util.Scanner;
 
 /*
- anti virus using dsf, bfs
+ anti virus using dsf, bfs, 완전 탐색.
  */
 public class GP04_14502 {
 
     static Scanner sc = new Scanner(System.in);
     static StringBuilder sb = new StringBuilder();
 
-    static int N, M, B, ans;
-    static int[][] A, blank;
-    static boolean[][] visit;
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static int N, M, B, ans; // 행 개수,열 개수, 빈공간 개수, 정답
+    static int[][] A, blank; // 공간정보 담김, 빈공간만 담김
+    static boolean[][] visit; // 방문 여부 체크
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // 격자 탐색 설정
 
     static void input(){
-        N = sc.nextInt();
-        M = sc.nextInt();
-        A = new int[N + 1][M + 1];
-        blank = new int[N * M + 1][2];
-        visit = new boolean[N + 1][M + 1];
+        N = sc.nextInt(); // 행 개수
+        M = sc.nextInt(); // 열 개수
+        A = new int[N + 1][M + 1]; // 공간 정보 : 0: 빈공간, 1: 벽, 2: 바이러스 감염
+        blank = new int[N * M + 1][2]; // 빈 공간 정보
+        visit = new boolean[N + 1][M + 1]; // 방문 여부
         for(int i = 1; i <= N; i++)
             for(int j = 1; j <= M; j++)
-                A[i][j] = sc.nextInt();
+                A[i][j] = sc.nextInt(); // 공간 정보 저장
     }
 
     static void bfs(){
@@ -59,13 +59,13 @@ public class GP04_14502 {
 
     }
     static void dfs(int idx,  int selected_cnt){
-        if(selected_cnt == 3){
-            bfs();
+        if(selected_cnt == 3){ // 빈공간 3개 선택, 벽을 3개만 세울 수 있다.
+            bfs(); // 빈공간 탐색
             return;
         }
-        if(idx > B) return;
+        if(idx > B) return; // 더이상 없으면 종료
 
-        A[blank[idx][0]][blank[idx][1]] = 1;
+        A[blank[idx][0]][blank[idx][1]] = 1;// 벽을 세움.
         dfs(idx +1, selected_cnt + 1);
 
         A[blank[idx][0]][blank[idx][1]] = 0;
@@ -74,16 +74,18 @@ public class GP04_14502 {
     }
 
     static void proCalc(){
+
+        // 전체 빈공간 탐색
         for(int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
-                if(A[i][j] == 0){
+                if(A[i][j] == 0){ // 0은 빈 공간
                     B++;
                     blank[B][0] = i;
                     blank[B][1] = j;
                 }
             }
         }
-        dfs(1, 0);
+        dfs(1, 0); // 벽을 세울지 여부 탐색
         System.out.println(ans);
     }
 
---
+++ src/GP05_2178.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/*
+미로 탈출, 경로 거리 계산 using bfs
+ */
+public class GP05_2178 {
+
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M; //
+
+    static String[] A;
+    static int[][] dir = {{1, 0},{0, 1},{-1, 0},{0, -1}};
+
+    static int[][] dist;
+    static boolean[][] visit ;
+    static ArrayList<Integer> group;
+
+    static void input(){
+        N = sc.nextInt();
+        M = sc.nextInt();
+        A = new String[N];
+        for (int i = 0; i < N; i++) A[i] = sc.next();
+        visit = new boolean [N][M];
+        dist = new int[N][M];
+
+    }
+
+    static void bfs(int x, int y){
+        for(int i = 0; i < N; i++) for(int j = 0; j < M ; j++) dist[i][j] = -1;
+
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(x);
+        Q.add(y);
+        dist[x][y] = 1;
+        visit[x][y] = true;
+
+        while(!Q.isEmpty()){
+            x = Q.poll();
+            y= Q.poll();
+            for(int k = 0 ; k < 4 ; k++){
+                int nx = x + dir[k][0], ny = y + dir[k][1];
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(A[nx].charAt(ny) == '0') continue;
+                if(visit[nx][ny]) continue;
+                Q.add(nx);
+                Q.add(ny);
+                visit[nx][ny] = true;
+                dist[nx][ny] = dist[x][y] + 1;
+            }
+        }
+
+    }
+    static void proCalc(){
+        bfs(0, 0);
+
+        System.out.println(dist[N -1][M - 1]);
+    }
+
+    public static void main(String[] args){
+        input();
+        proCalc();
+    }
+
+}
